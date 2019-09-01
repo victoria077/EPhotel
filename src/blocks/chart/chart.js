61dea6d7f1c76ecdb332b0fe375d8db2b2b79b37
viktoria <vik.slepchenko@gmail.com>
2019-09-01 23:44:08 +0200

    add material icon and block with information about room

---
+++ src/blocks/chart/chart.js
@@ -0,0 +1,44 @@
+// var ctx = $("myChart").get(0).getContext('2d');
+// var orange_gradient = ctx.createLinearGradient(0, 0, 0, 100);
+// orange_gradient.addColorStop(0, '#FFE39C');
+// orange_gradient.addColorStop(1, ' #FFBA9C');
+// var blue_gradient = ctx.createLinearGradient(0, 0, 0, 400);
+// blue_gradient.addColorStop(0, '#BC9CFF');
+// blue_gradient.addColorStop(1, ' #8BA4F9');
+// var green_gradient = ctx.createLinearGradient(0, 0, 0, 500);
+// green_gradient.addColorStop(0, '#6FCF97');
+// green_gradient.addColorStop(1, '  #66D2EA');
+// var gray_gradient = ctx.createLinearGradient(0, 0, 0, 500);
+// gray_gradient.addColorStop(0, ' #919191');
+// gray_gradient.addColorStop(1, '#3D4975');
+// var myChart = new Chart(ctx, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ["Великолепно",
+// "Хорошо",
+// "Удовлетворительно",
+// "Разочарован"],
+//     datasets: [{
+//       backgroundColor: [
+//         blue_gradient,
+//         green_gradient,
+//         orange_gradient,
+//         gray_gradient
+//       ],
+//       data: [25, 25, 50, 0]
+//     }]
+//   },
+//    options: {
+//             cutoutPercentage: 80,
+//             responsive: true,
+//             legend: {
+//                 position: 'right',
+//                 labels: {
+//                     usePointStyle:true,
+//                     fontColor: "black",
+//                     boxWidth: 20,
+//                     padding: 20,
+//                 }
+//             }
+//         }
+// });
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@
 import "./scss/main.scss";
 import "./index.pug";
 import "../src/components/signin-form/signin-form.pug"
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+// import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
 import "../src/components/form-search/form-search.js";
 import "../src/components/components-page/components-page.js";
 import "../src/components/datepicker/datepicker.js";
 import "../src/components/slider/slider.js";
+import "../src/blocks/chart/chart.js";
 require('./fonts.scss');
 require('jquery');
 require('jquery-ui-bundle');
@@ -20,6 +21,7 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 import "../src/components/card-room/card-room.js";
 import "../src/components/room-page/room-page.js";
+import "../node_modules/material-icons/css/material-icons.min.css"
 
 window.onload = function(){
     var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
