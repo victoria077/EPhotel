fd4e4ac94c92ae2bfe9aa980eba152724b9743eb
viktoria <vik.slepchenko@gmail.com>
2019-09-09 10:48:59 +0200

    add changes in size of card room and titles

--- src/components/room-page/room-page.js
+++ src/components/room-page/room-page.js
@@ -1,33 +1,4 @@
 
-// window.onload = function(){
-  
-//     var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
-//     $('.cdp_i').on('click', function(){
-//       var go = $(this).attr('href').replace('#!', '');
-//       if (go === '+1') {
-//         paginationPage++;
-//       } else if (go === '-1') {
-//         paginationPage--;
-//       }else{
-//         paginationPage = parseInt(go, 10);
-//       }
-//       $('.cdp').attr('actpage', paginationPage);
-//     });
-//   };
-
-  // $( function() {
-  //   $( "#slider-range" ).slider({
-  //     range: true,
-  //     min: 1000,
-  //     max: 15000,
-  //     values: [ 5000, 10000 ],
-  //     slide: function( event, ui ) {
-  //       $( "#amount" ).val( "₽" + ui.values[ 0 ] + " - ₽" + ui.values[ 1 ] );
-  //     }
-  //   });
-  //   $( "#amount" ).val( "₽" + $( "#slider-range" ).slider( "values", 0 ) +
-  //     " - ₽" + $( "#slider-range" ).slider( "values", 1 ) );
-  // } );
 
   $(document).ready(function() { 
     $(".close").click(function() { 
@@ -50,18 +21,3 @@
   });  
 }); 
 
-
-// window.onload = function(){
-//   console.log("kkkkk")
-//   function closeSideBar(){
-//     document.getElementById("sidebar").style.width = "0";
-//     document.getElementById("left-side").style.marginLeft = "0";
-//     console.log("fgfhg");
-//   }
-
-
-//   function openSideBar(){
-//     document.getElementById("sidebar").style.width = "250px";
-//     document.getElementById("left-side").style.marginLeft = "250px";
-//   }
-// }
