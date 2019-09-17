ecabb5dd9e1ad66fed705a8a7567e476ffe1f4fa
viktoria <vik.slepchenko@gmail.com>
2019-09-17 02:45:36 +0200

    add background on info-room and images on filter-room

--- src/blocks/littleDropdown/littleDropdown.js
+++ src/blocks/littleDropdown/littleDropdown.js
@@ -1,65 +1,65 @@
-let valueAd, valueChild, valueBaby;
-$("#adult_plus").on("click", function(){
-    let newScore = $("#adult_text").html();
-    valueAd = parseInt(newScore) + 1;
-    $("#adult_text").html(valueAd); 
-    return valueAd;
-})
+// let valueAd, valueChild, valueBaby;
+// $("#adult_plus").on("click", function(){
+//     let newScore = $("#adult_text").html();
+//     valueAd = parseInt(newScore) + 1;
+//     $("#adult_text").html(valueAd); 
+//     return valueAd;
+// })
 
-$("#adult_minus").on("click", function(){
+// $("#adult_minus").on("click", function(){
   
-    let newScore = $("#adult_text").html();
-    if(newScore > 0) {
-    valueAd = parseInt(newScore) - 1;
-    $("#adult_text").html(valueAd); }
-    return valueAd;
-  }                       
-)
+//     let newScore = $("#adult_text").html();
+//     if(newScore > 0) {
+//     valueAd = parseInt(newScore) - 1;
+//     $("#adult_text").html(valueAd); }
+//     return valueAd;
+//   }                       
+// )
 
-$("#child_plus").on("click", function(){
-    let newScore = $("#child_text").html();
-    valueChild= parseInt(newScore) + 1;
-    $("#child_text").html(valueChild); 
-    return valueChild;
-})
+// $("#child_plus").on("click", function(){
+//     let newScore = $("#child_text").html();
+//     valueChild= parseInt(newScore) + 1;
+//     $("#child_text").html(valueChild); 
+//     return valueChild;
+// })
 
-$("#child_minus").on("click", function(){
-    let newScore = $("#child_text").html();
-   if(newScore > 0) {
-    valueChild = parseInt(newScore) - 1;
-    $("#child_text").html(valueChild); }
-    return valueChild;
-  }                       
-)
+// $("#child_minus").on("click", function(){
+//     let newScore = $("#child_text").html();
+//    if(newScore > 0) {
+//     valueChild = parseInt(newScore) - 1;
+//     $("#child_text").html(valueChild); }
+//     return valueChild;
+//   }                       
+// )
 
-$("#baby_plus").on("click", function(){
-    let newScore = $("#baby_text").html();
-    valueBaby = parseInt(newScore) + 1;
-    $("#baby_text").html(valueBaby); 
-    return valueBaby;
-})
+// $("#baby_plus").on("click", function(){
+//     let newScore = $("#baby_text").html();
+//     valueBaby = parseInt(newScore) + 1;
+//     $("#baby_text").html(valueBaby); 
+//     return valueBaby;
+// })
 
-$("#baby_minus").on("click", function(){
-    let newScore = $("#baby_text").html();
-   if(newScore > 0) {
-    valueBaby = parseInt(newScore) - 1;
-    $("#baby_text").html(valueBaby); }
-    return valueBaby;
-  }                       
-)
+// $("#baby_minus").on("click", function(){
+//     let newScore = $("#baby_text").html();
+//    if(newScore > 0) {
+//     valueBaby = parseInt(newScore) - 1;
+//     $("#baby_text").html(valueBaby); }
+//     return valueBaby;
+//   }                       
+// )
 
-$("#clean").on("click", function(){
-  $("#baby_text").html(0); 
-  $("#child_text").html(0); 
-  $("#adult_text").html(0); 
-});
+// $("#clean").on("click", function(){
+//   $("#baby_text").html(0); 
+//   $("#child_text").html(0); 
+//   $("#adult_text").html(0); 
+// });
 
-$("#clean").on("click", function(){
-  $("#baby_text").html(0); 
-  $("#child_text").html(0); 
-  $("#adult_text").html(0); 
-});
+// $("#clean").on("click", function(){
+//   $("#baby_text").html(0); 
+//   $("#child_text").html(0); 
+//   $("#adult_text").html(0); 
+// });
 
-$("#baby_plus").on("click", function(){
-    console.log("sdaf");
-})
+// $("#baby_plus").on("click", function(){
+//     console.log("sdaf");
+// })
