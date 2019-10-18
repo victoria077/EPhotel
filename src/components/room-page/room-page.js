a62e5bc8eb0586c0687756da66ad24aeff577278
viktoria <vik.slepchenko@gmail.com>
2019-10-18 12:28:54 +0200

    change styles for footer and buttons

--- src/components/room-page/room-page.js
+++ src/components/room-page/room-page.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   $('.close').click(function() {
     $('#sidebar').hide();
-    $('#left-side').width('0');
+    $('.left-side').width('0');
     $('.right-side').show();
     $('.right-side').width('100%');
   });
   $('.slide').click(function() {
     $('#sidebar').show();
-    $('#left-side').css('display', 'block');
-    $('#left-side').addClass('show');
-    $('#left-side').width('700px');
+    $('.left-side').css('display', 'block');
+    $('.left-side').addClass('show');
+    $('.left-side').width('700px');
     $('.right-side').hide();
     $('.right-side').width('0');
   });
