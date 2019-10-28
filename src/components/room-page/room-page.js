af952a6697b6b15ae45e7b7d8c92af61556bf3d3
viktoria <vik.slepchenko@gmail.com>
2019-10-28 12:56:01 +0100

    modify styles for room page

--- src/components/room-page/room-page.js
+++ src/components/room-page/room-page.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $('.close').click(function() {
     $('#sidebar').hide();
-    $('.left-side').width('0');
-    $('.right-side').show();
-    $('.right-side').width('100%');
+    $('.settings__left-side').width('0');
+    $('.settings__right-side').show();
+    $('.settings__right-side').width('100%');
   });
   $('.slide').click(function() {
     $('#sidebar').show();
-    $('.left-side').css('display', 'block');
-    $('.left-side').addClass('show');
-    $('.left-side').width('700px');
-    $('.right-side').hide();
-    $('.right-side').width('0');
+    $('.settings__left-side').css('display', 'block');
+    $('.settings__left-side').addClass('show');
+    $('.settings__left-side').width('700px');
+    $('.settings__right-side').hide();
+    $('.settings__right-side').width('0');
   });
 });
