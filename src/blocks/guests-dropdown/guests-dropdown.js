e7be1d47d97b90d48aa865920c15e4caf0d10492
viktoria <vik.slepchenko@gmail.com>
2019-11-28 14:56:10 +0100

    fixed guests dropdown

--- src/blocks/guests-dropdown/guests-dropdown.js
+++ src/blocks/guests-dropdown/guests-dropdown.js
@@ -1,21 +1,40 @@
 var bedroom = 0;
 var bed = 0;
 var bathroom = 0;
+
+function num2str(n, text_forms) {  
+  n = Math.abs(n) % 100; var n1 = n % 10;
+  if (n > 10 && n < 20) { return text_forms[2]; }
+  if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+  if (n1 == 1) { return text_forms[0]; }
+  return text_forms[2];
+}
+
+function countComfort() {
+  if(bedroom > 0 && bed > 0 && bathroom > 0) {
+    $('#comfort-button').html(bedroom + ' ' +num2str(bedroom, [ 'спальня', 'спальни', 'спален']) + ',' + ' ' + bed + ' ' +num2str(bed, ['кровать', 'кровати', 'кроватей']) + ',' + ' ' + '...' );
+  } 
+
+  else if(bedroom > 0 && bed > 0) {
+    $('#comfort-button').html(bedroom + ' ' +num2str(bedroom, [ 'спальня', 'спальни', 'спален']) + ',' + ' ' + bed + ' ' +num2str(bed, ['кровать', 'кровати', 'кроватей']));
+  }
+
+  else if(bedroom > 0 && bathroom > 0) {
+    $('#comfort-button').html(bedroom + ' ' +num2str(bedroom, [ 'спальня', 'спальни', 'спален'])  + ',' + ' ' + bathroom + ' ' +num2str(bathroom, ['ванная', 'ванных', 'ванных']));
+  }
+  else if (bedroom > 0) {
+    $('#comfort-button').html(bedroom + ' ' +num2str(bedroom, [ 'спальня', 'спальни', 'спален']));
+  }
+  else{
+    $('#comfort-button').html('Удобства');
+  }
+}
+
 $("#bedroom_plus").on("click", function() {
   let newScore = $("#bedroom_text").html();
   bedroom = parseInt(newScore) + 1;
   $("#bedroom_text").html(bedroom);
-  if (bed > 0) {
-    $("#comfort-button").html(
-      bedroom + " " + "спален" + "," + " " + bed + " " + "кровати"
-    );
-  } else if (bathroom > 0) {
-    $("#comfort-button").html(
-      bedroom + " " + "спален" + "," + " " + bathroom + " " + "ванные"
-    );
-  } else {
-    $("#comfort-button").html(bedroom + " " + "спален");
-  }
+  countComfort();
   return bedroom;
 });
 
@@ -23,19 +42,9 @@ $("#bedroom_minus").on("click", function() {
   let newScore = $("#bedroom_text").html();
   if (newScore > 0) {
     bedroom = parseInt(newScore) - 1;
-    $("#bedroom_text").html(bedroom);
-    if (bed > 0) {
-      $("#comfort-button").html(
-        bedroom + " " + "спален" + "," + " " + bed + " " + "кровати"
-      );
-    } else if (bathroom > 0) {
-      $("#comfort-button").html(
-        bedroom + " " + "спален" + "," + " " + bathroom + " " + "ванные"
-      );
-    } else {
-      $("#comfort-button").html(bedroom + " " + "спален");
-    }
+    $("#bedroom_text").html(bedroom);  
   }
+  countComfort();
   return bedroom;
 });
 
@@ -43,6 +52,7 @@ $("#bed_plus").on("click", function() {
   let newScore = $("#bed_text").html();
   bed = parseInt(newScore) + 1;
   $("#bed_text").html(bed);
+  countComfort();
   return bed;
 });
 
@@ -52,6 +62,7 @@ $("#bed_minus").on("click", function() {
     bed = parseInt(newScore) - 1;
     $("#bed_text").html(bed);
   }
+  countComfort();
   return bed;
 });
 
@@ -59,6 +70,7 @@ $("#bathroom_plus").on("click", function() {
   let newScore = $("#bathroom_text").html();
   bathroom = parseInt(newScore) + 1;
   $("#bathroom_text").html(bathroom);
+  countComfort();
   return bathroom;
 });
 
@@ -68,19 +80,7 @@ $("#bathroom_minus").on("click", function() {
     bathroom = parseInt(newScore) - 1;
     $("#bathroom_text").html(bathroom);
   }
+  countComfort();
   return bathroom;
 });
 
-$("#comfort-clean").on("click", function() {
-  $("#bedroom_text").html(0);
-  $("#bed_text").html(0);
-  $("#bathroom_text").html(0);
-  $("#comfort-button").html("Удобства");
-  bedroom = 0;
-  bed = 0;
-  bathroom = 0;
-});
-
-$("#comfort-apply").on("click", function() {
-  $("#clean").css("color", "rgba(31, 32, 65, 0.5)");
-});
